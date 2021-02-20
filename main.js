13f54ed921fb6a8b6426d850f1dce030cbcbfc73
bayrakhakan01 <76584714+bayrakhakan01@users.noreply.github.com>
2021-02-20 22:29:50 +0100

    update

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-
 const newTask = document.querySelector("#new-task-input");
 const addTaskBtn = document.querySelector(".add-task-btn");
 const removeCompleteBtn = document.querySelector(".remove-complete-btn");
